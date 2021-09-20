8402f31d4ebaffb4622aeb588c124e7028b74587
Shreyansh Raj <hrishiraj1570@gmail.com>
2021-09-20 11:34:38 +0530

    Added news

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from flask import Flask, render_template, request, jsonify
-import requests
-import json
-
-api = 'https://newsapi.org/v2/everything?apikey=2f8eda0083fc4be29257842fa81d73a0'
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    params={'q':'everything'}
-    data = requests.get(url=api,params=params).json()
-    return render_template('index.html',data=data)
+    query = "everything"
+    return render_template('index.html',query=query)
 
 if __name__=='__main__':
     app.run(debug=True)
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,10 +1,29 @@
-console.log(data);
-articles = data['articles']
-for (let index = 0; index < 15; index++){
-    console.log("title: "+articles[index]['title']+"\n"+"description: "+articles[index]['description']+"\n")
-}
+console.log(query);
+let api = `https://newsapi.org/v2/everything?apikey=2f8eda0083fc4be29257842fa81d73a0&q=${query}`;
+console.log(api);
+var data;
+fetch(api).then((response) => response.json().then(responseJSON => data = responseJSON).then(() => {
+    console.log(data);
+    var articles = data["articles"];
+    for (let index = 0; index < 15; index++) {
+        let article = articles[index];
+        let image = article["urlToImage"];
+        let title = article["title"];
+        let description = article["description"];
+        let link = article["url"];
+        let card = `
+        <div class="news-card">
+        <img src="${image}" class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${description}</p>
+            <a href="${link}" target="_blank" class="card-button">Read More</a>
+        </div>
+        </div>`;
+        document.getElementById("news-container").innerHTML += card;
+    }
 
-let card = `
+    let card = `
         <div class="news-card">
         <img src="https://source.unsplash.com/user/erondu/1600x900" class="card-img-top" alt="..."/>
         <div class="card-body">
@@ -13,3 +32,4 @@ let card = `
             <a href="#" target="_blank" class="card-button">Read More</a>
         </div>
         </div>`;
+}));
